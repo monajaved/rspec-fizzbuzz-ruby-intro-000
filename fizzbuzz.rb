1ee2d55f6c764ec66696957dff293fef85dd3160
Mona Javed Syed <monajaved93@gmail.com>
2020-10-16 04:21:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,11 +4,11 @@
 
 
 def fizzbuzz (int)
-  if int % 15 == 0 
+  if int % 3 == 0
     return "FizzBuzz"
   elsif int % 5 == 0
     return "Buzz"
-  elsif int % 3 == 0
+  elsif int % 15 == 0
     return "Fizz"
   else
     return nil
